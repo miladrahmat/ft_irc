717503ef1fa6fc1ba35114ff5331dafb3bfaf63a
Milad Rahmat Abadi <milad.rahmat@gmail.com>
2025-06-13 15:35:15 +0300

    Added more attributes to Client, added getters and setters for all the private attributes

--- includes/Client.hpp
+++ includes/Client.hpp
@@ -5,15 +5,27 @@
 class Client {
 	private:
 		int			_client_socket;
-		std::string	_name;
+		std::string	_real_name;
 		std::string	_nickname;
+		std::string	_user_name;
+		std::string	_host_name;
+		std::string	_buffer;
 
 	public:
 		Client(int socket);
 		Client(Client&& old_client);
 		~Client();
 		int			getClientSocket() const;
-		std::string	getName() const;
+		std::string	getRealName() const;
 		std::string	getNickname() const;
+		std::string	getUserName() const;
+		std::string	getHostName() const;
+		std::string	getBuffer() const;
 		void		setClientSocket(int socket);
+		void		setRealName(std::string& real_name);
+		void		setUserName(std::string& user_name);
+		void		setNickName(std::string& nickname);
+		void		setHostName(std::string& host_name);
+		void		appendBuffer(std::string& msg);
+
 };
--- sources/Client.cpp
+++ sources/Client.cpp
@@ -5,7 +5,8 @@ Client::Client(int socket) : _client_socket(socket) {
 
 }
 
-Client::Client(Client&& old_client) : _client_socket(old_client._client_socket), _name(old_client._name), _nickname(old_client._nickname) {
+Client::Client(Client&& old_client) : _client_socket(old_client._client_socket), _real_name(old_client._real_name), _nickname(old_client._nickname), \
+										_user_name(old_client._user_name), _host_name(old_client._host_name), _buffer(old_client._buffer) {
 	old_client.setClientSocket(-1);
 }
 
@@ -17,14 +18,46 @@ int	Client::getClientSocket() const {
 	return (_client_socket);
 }
 
-std::string	Client::getName() const {
-	return (_name);
+std::string	Client::getRealName() const {
+	return (_real_name);
 }
 
 std::string	Client::getNickname() const {
 	return (_nickname);
 }
 
+std::string	Client::getUserName() const {
+	return (_user_name);
+}
+
+std::string	Client::getHostName() const {
+	return (_host_name);
+}
+
+std::string	Client::getBuffer() const {
+	return (_buffer);
+}
+
 void	Client::setClientSocket(int socket) {
 	_client_socket = socket;
+}
+
+void	Client::setUserName(std::string& user_name) {
+	_user_name = user_name;
+}
+
+void	Client::setRealName(std::string& real_name) {
+	_real_name = real_name;
+}
+
+void	Client::setNickName(std::string& nickname) {
+	_nickname = nickname;
+}
+
+void	Client::setHostName(std::string& host_name) {
+	_host_name = host_name;
+}
+
+void	Client::appendBuffer(std::string& msg) {
+	_buffer.append(msg);
 }
