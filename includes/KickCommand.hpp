4173540b3b20fcae95cce3575c9b3b4bf6bbaafb
Maria <maria.helenius99@gmail.com>
2025-07-17 10:53:35 +0300

    Cleaned up KickCommand class

--- includes/KickCommand.hpp
+++ includes/KickCommand.hpp
@@ -4,12 +4,12 @@
 
 class KickCommand : public ACommand {
     private:
-        std::string  _channel;
+        std::string _channel;
         std::string _client_to_kick;
         std::string _msg;
         KickCommand(std::string command, std::shared_ptr<Client>& client, State& state);
     public:
-        static std::unique_ptr<ACommand> create(std::string command, std::shared_ptr<Client>& client, State& state,
-			std::vector<std::string> args);
-        void    execute() const override;
+        static std::unique_ptr<ACommand> create(std::string command, std::shared_ptr<Client>& client,
+            State& state, std::vector<std::string> args);
+        void execute() const override;
 };
