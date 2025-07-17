135a49b4a3387173c21fb40def39b6c0d72ee9f2
Maria <maria.helenius99@gmail.com>
2025-07-17 11:22:20 +0300

    Cleaned up QuitCommand class

--- includes/QuitCommand.hpp
+++ includes/QuitCommand.hpp
@@ -1,3 +1,4 @@
+
 #pragma once
 
 #include "ACommand.hpp"
@@ -7,8 +8,9 @@ class QuitCommand : public ACommand {
     private:
         std::string _msg;
         QuitCommand(std::string command, std::shared_ptr<Client>& client, State& state);
+
     public:
-        static std::unique_ptr<ACommand> create(std::string command, std::shared_ptr<Client>& client, State& state,
-			std::string args);
-        void    execute() const override;
+        static std::unique_ptr<ACommand> create(std::string command, std::shared_ptr<Client>& client,
+            State& state, std::string args);
+        void execute() const override;
 };
