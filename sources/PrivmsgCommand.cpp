b2de29c8137355445e8c09da3a856c9df9376309
Milad Rahmat Abadi <milad.rahmat@gmail.com>
2025-07-22 13:07:06 +0300

    Cleaning up: Removed unnecessary code

--- sources/PrivmsgCommand.cpp
+++ sources/PrivmsgCommand.cpp
@@ -10,7 +10,7 @@ std::unique_ptr<ACommand> PrivmsgCommand::create(std::string command, std::share
 	PrivmsgCommand*	cmd = new PrivmsgCommand(command, client, state);
 	cmd->_msg_to = target;
 	cmd->_msg = msg.substr(1, msg.length());
-	if (cmd->_msg_to[0] == '#' || cmd->_msg_to[0] == '&') {
+	if (cmd->_msg_to[0] == '#') {
 		std::vector<Channel>::iterator channel = cmd->_state.getChannel(cmd->_msg_to);
 		if (channel == cmd->_state.getChannels().end()) {
 			delete cmd;
